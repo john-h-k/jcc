3334d054852f620da76bc4231d99a612f2be3f26
John Kelly <johnharrykelly@gmail.com>
2024-05-05 21:06:09 +0100

    graphcol WIP

--- lsra.h
+++ lsra.h
@@ -1,17 +1,15 @@
 #ifndef LSRA_H
 #define LSRA_H
 
-#include "alloc.h"
-#include "ir/build.h"
+#include <stddef.h>
 
-#include <limits.h>
-#include <stdlib.h>
+#include "ir/build.h"
 
 struct reg_info {
   size_t num_regs;
 };
 
 void print_ir_intervals(FILE *file, struct ir_op *op, void *metadata);
-void register_alloc(struct ir_builder *irb, struct reg_info reg_info);
+void lsra_register_alloc(struct ir_builder *irb, struct reg_info reg_info);
 
 #endif
