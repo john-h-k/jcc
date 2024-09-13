b424eb655bed2cc1585f1b257ff02f4f2cc70f2d
John Kelly <johnharrykelly@gmail.com>
2024-09-13 23:07:47 +0100

    Update variadic.c

--- tests/variadic.c
+++ tests/variadic.c
@@ -2,8 +2,11 @@
 // stdout: "Hello, World!"
 
 int printf(const char *format, ...);
+int puts(const char *s);
 
 int main() {
-  printf("%s. Numbers work too - %d!\n", "Hello, World!", 2);
+  printf("%s. Numbers work too - %d!", "Hello, World!", 2);
+  puts("");
 }
+
 
