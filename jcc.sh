5cd19ed98d876aca26693ca3f7f03cadb3d86935
John Kelly <johnharrykelly@gmail.com>
2024-09-14 17:57:41 +0100

    tabs

--- jcc.sh
+++ jcc.sh
@@ -1,68 +1,73 @@
 #!/bin/zsh
 
+build() {
+
+    cmake -DCMAKE_BUILD_TYPE=debug -DCMAKE_EXE_LINKER_FLAGS="-fsanitize=undefined" -B build -S . && cmake --build .
+}
+
 cfg() {
-  "./$(dirname $0)/build/jcc" "$@"
-  for file in $(find $(dirname $0)/build -name '*.gv' -print); do
-    name=$(basename $file)
-    dot -Tpng "$(dirname $0)/$file" > "$name.png" && open "$name.png"
-  done
+    "./$(dirname $0)/build/jcc" "$@"
+    for file in $(find $(dirname $0)/build -name '*.gv' -print); do
+        name=$(basename $file)
+        dot -Tpng "$(dirname $0)/$file" > "$name.png" && open "$name.png"
+    done
 }
 
 format() {
-  # if [ $(git status --short | wc -l) != 0 ]; then
-  #   echo "cannot format when changes are present! commit/stash/discard first"
-  #   exit -1
-  # fi
+    if [ $(git status --short | wc -l) != 0 ]; then
+        echo "cannot format when changes are present! commit/stash/discard first"
+        exit -1
+    fi
 
-  echo "Formatting..."
-  fd '.*\.[hc]' . -x clang-format -style=file -i
+    echo "Formatting..."
+    fd '.*\.[hc]' . -x clang-format -style=file -i
 }
 
 invoke-subcommand() {
-  local base name func
+    local base name func
 
-  base=$1
+    base=$1
 
-  unset name
-  if [ -z "${1}" ]; then
-      echo "No subcommand provided; defaulting to 'help' subcommand..."
-      name="help"
-  fi
+    unset name
+    if [ -z "${1}" ]; then
+        echo "No subcommand provided; defaulting to 'help' subcommand..."
+        name="help"
+    fi
 
-  func=${name:="${1}"}
+    func=${name:="${1}"}
 
-  if declare -f "${func}" >/dev/null 2>&1; then
-      shift 1 >/dev/null 2>&1
+    if declare -f "${func}" >/dev/null 2>&1; then
+        shift 1 >/dev/null 2>&1
 
-      "${func}" "${@}"
+        "${func}" "${@}"
 
-      if [ $? != 0 ]; then
-          return $?
-      fi
-  elif [[ $name == "help" ]]; then
-      local func_names=(${(Mok)functions:#$base?*}) # don't match private _ lead functions
+        if [ $? != 0 ]; then
+            return $?
+        fi
+    elif [[ $name == "help" ]]; then
+        local func_names=(${(Mok)functions:#$base?*}) # don't match private _ lead functions
 
-      echo "Usage: ${base} <subcommand> [options]"
-      echo "Subcommands:"
-      for func in $func_names; do
-          print "    - $base ${func#${base}-}"
-      done
-  else
-      local matches=(${(Mok)functions:#$base-$name*})
+        echo "Usage: ${base} <subcommand> [options]"
+        echo "Subcommands:"
+        for func in $func_names; do
+            print "    - $base ${func#${base}-}"
+        done
+    else
+        local matches=(${(Mok)functions:#$base-$name*})
 
-      if [[ ${#matches[@]} == 1 ]]; then
-          "${matches[1]}" "${@}"
-      elif [[ ${#matches[@]} > 1 ]]; then
-          echo "'${name}' is ambiguous; did you mean one of the following?" >&2
-          for match in $matches; do
-              echo "    - $base ${match#${base}-}" >&2
-          done
-          return 1
-      else
-          echo "'${name}' - not valid subcommand for '${base}'" >&2
-          return 1
-      fi
-  fi
+        if [[ ${#matches[@]} == 1 ]]; then
+            "${matches[1]}" "${@}"
+        elif [[ ${#matches[@]} > 1 ]]; then
+            echo "'${name}' is ambiguous; did you mean one of the following?" >&2
+            for match in $matches; do
+                echo "    - $base ${match#${base}-}" >&2
+            done
+            return 1
+        else
+            echo "'${name}' - not valid subcommand for '${base}'" >&2
+            return 1
+        fi
+    fi
 }
 
 invoke-subcommand "$@"
