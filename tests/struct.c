35123dbb28a9fb018ae6b14d0e21ea57937a1953
John Kelly <johnharrykelly@gmail.com>
2024-09-18 21:58:32 +0100

    Nested structs

--- tests/struct.c
+++ tests/struct.c
@@ -1,13 +1,20 @@
-// expected value: 25
+// expected value: 50
+
+struct bar {
+  int z;
+};
 
 struct foo {
   int a, b;
   char *c;
   unsigned long d[3];
+
+  struct bar e;
 };
 
 int main() {
   struct foo bar;
   bar.a = 25;
-  return bar.a;
+  bar.e.z = 50;
+  return bar.e.z;
 }
