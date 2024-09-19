b174fb6b5da79c8cae33fddc510ce7c32d666956
John Kelly <johnharrykelly@gmail.com>
2024-09-19 16:34:27 +0100

    Array usage as var fixes
    ```c
    int a[4];
    int *p = a;
    ```
    means that when reading an array we need to actually address it

---
+++ tests/fancy_pointer.c
@@ -0,0 +1,14 @@
+// expected value: 10
+
+int main() {
+  int a[4];
+  int *p = a;
+
+  *(p + 0) = 1;
+  *(p + 1) = 2;
+  *(p + 2) = 3;
+  *(p + 3) = (p + 4) - (p);
+
+
+  return p[0] + p[1] + p[2] + p[3];
+}
