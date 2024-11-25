bd8ba6e615d48219a7d7214ce640a120300a5ca8
John Kelly <johnharrykelly@gmail.com>
2024-11-25 21:46:54 +0000

    Mandelbrot working

--- ir/eliminate_phi.c
+++ ir/eliminate_phi.c
@@ -14,29 +14,37 @@ static void remove_critical_edges(struct ir_func *irb) {
   while (basicblock) {
     size_t num_preds = basicblock->num_preds;
 
+    struct ir_stmt *phi_stmt = NULL;
+    if (basicblock->first && basicblock->first->first &&
+        basicblock->first->first->ty == IR_OP_TY_PHI) {
+      phi_stmt = basicblock->first;
+    }
+
     if (num_preds > 1) {
       for (size_t i = 0; i < num_preds; i++) {
         struct ir_basicblock *pred = basicblock->preds[i];
 
-        if (pred->ty != IR_BASICBLOCK_TY_SWITCH &&
-            pred->ty != IR_BASICBLOCK_TY_SPLIT) {
-          continue;
-        }
-
-        bug("crit edges");
         // we have a critical edge
         struct ir_basicblock *intermediate =
             insert_before_ir_basicblock(irb, basicblock);
+        intermediate->comment = "intermediate";
         intermediate->ty = IR_BASICBLOCK_TY_MERGE;
         intermediate->merge =
             (struct ir_basicblock_merge){.target = basicblock};
 
-        struct ir_stmt *stmt = alloc_ir_stmt(irb, intermediate);
-        struct ir_op *op = alloc_ir_op(irb, stmt);
+        struct ir_stmt *intermediate_phi_stmt = NULL;
+        if (phi_stmt) {
+          intermediate_phi_stmt = alloc_ir_stmt(irb, intermediate);
+        }
+
+        struct ir_stmt *br_stmt = alloc_ir_stmt(irb, intermediate);
+        struct ir_op *op = alloc_ir_op(irb, br_stmt);
         op->ty = IR_OP_TY_BR;
         op->var_ty = IR_VAR_TY_NONE;
 
         basicblock->preds[i] = intermediate;
+
+        add_pred_to_basicblock(irb, intermediate, pred);
 
         switch (pred->ty) {
         case IR_BASICBLOCK_TY_SPLIT:
@@ -54,8 +62,41 @@ static void remove_critical_edges(struct ir_func *irb) {
             }
           }
           break;
-        default:
+        case IR_BASICBLOCK_TY_MERGE:
+          pred->merge.target = intermediate;
+          break;
+        case IR_BASICBLOCK_TY_RET:
           unreachable();
+        }
+
+        if (phi_stmt) {
+          struct ir_op *phi = phi_stmt->first;
+          while (phi && phi->ty == IR_OP_TY_PHI) {
+            printf("checking phi %zu\n", phi->id);
+
+            struct ir_op *int_phi = alloc_ir_op(irb, intermediate_phi_stmt);
+            int_phi->ty = IR_OP_TY_PHI;
+            int_phi->var_ty = phi->var_ty;
+            int_phi->reg = phi->reg;
+            int_phi->phi = (struct ir_op_phi){
+                .num_values = 1,
+                .values =
+                    arena_alloc(irb->arena, sizeof(*int_phi->phi.values))};
+
+            for (size_t j = 0; j < phi->phi.num_values; j++) {
+              struct ir_phi_entry *entry = &phi->phi.values[j];
+
+              if (entry->basicblock == pred) {
+                int_phi->phi.values[0] = (struct ir_phi_entry){
+                    .basicblock = pred, .value = entry->value};
+                *entry = (struct ir_phi_entry){.basicblock = intermediate,
+                                               .value = int_phi};
+                break;
+              }
+            }
+
+            phi = phi->succ;
+          }
         }
       }
     }
@@ -70,8 +111,7 @@ struct bb_reg {
 };
 
 static void gen_moves(struct ir_func *irb, struct ir_basicblock *basicblock,
-                      struct hashtbl *reg_to_val,
-                       struct move_set moves,
+                      struct hashtbl *reg_to_val, struct move_set moves,
                       size_t tmp_index, struct ir_lcl *spill_lcl,
                       bool early_moves) {
   struct ir_op *last =
@@ -82,12 +122,7 @@ static void gen_moves(struct ir_func *irb, struct ir_basicblock *basicblock,
   for (size_t j = 0; j < moves.num_moves; j++) {
     struct move move = moves.moves[j];
 
-    // struct ir_reg from = reg_for_unique_idx(move.from);
     struct ir_reg to = reg_for_unique_idx(move.to);
-
-    printf("ANS from %zu -> %zu\n", move.from, move.to);
-
-    // printf("from %zu -> %zu\n", from.idx, to.idx);
 
     struct ir_op *value = NULL;
     if (move.from != tmp_index) {
@@ -249,12 +284,14 @@ void eliminate_phi(struct ir_func *irb) {
       struct move_set gp_moves = gen_move_order(
           irb->arena, vector_head(gp_move_from), vector_head(gp_move_to),
           vector_length(gp_move_from), tmp_index);
-      gen_moves(irb, basicblock, reg_to_val, gp_moves, tmp_index, spill_lcl, early);
+      gen_moves(irb, basicblock, reg_to_val, gp_moves, tmp_index, spill_lcl,
+                early);
 
       struct move_set fp_moves = gen_move_order(
           irb->arena, vector_head(fp_move_from), vector_head(fp_move_to),
           vector_length(fp_move_from), tmp_index);
-      gen_moves(irb, basicblock, reg_to_val, fp_moves, tmp_index, spill_lcl, early);
+      gen_moves(irb, basicblock, reg_to_val, fp_moves, tmp_index, spill_lcl,
+                early);
     }
   }
 }
--- tests/programs/mandelbrot.c
+++ tests/programs/mandelbrot.c
@@ -2,7 +2,6 @@
 int printf(const char *, ...);
 
 int mandelbrot(double real, double imag) {
-	return 0;
 	int limit = 100;
 	double zReal = real;
 	double zImag = imag;
@@ -44,53 +43,52 @@ int main() {
 	double dx = (x_fin - x_start)/(width - 1);
 	double dy = (y_fin - y_start)/(height - 1);
 
-	// const char *char_ = "\u2588";
+	const char *char_ = "\u2588";
 
-	// const char *black = "\033[22;30m";
-	// const char *red = "\033[22;31m";
-	// const char *l_red = "\033[01;31m";
-	// const char *green = "\033[22;32m";
-	// const char *l_green = "\033[01;32m";
-	// const char *orange = "\033[22;33m";
-	// const char *yellow = "\033[01;33m";
-	// const char *blue = "\033[22;34m";
-	// const char *l_blue = "\033[01;34m";
-	// const char *magenta = "\033[22;35m";
-	// const char *l_magenta = "\033[01;35m";
-	// const char *cyan = "\033[22;36m";
-	// const char *l_cyan = "\033[01;36m";
+	const char *black = "\033[22;30m";
+	const char *red = "\033[22;31m";
+	const char *l_red = "\033[01;31m";
+	const char *green = "\033[22;32m";
+	const char *l_green = "\033[01;32m";
+	const char *orange = "\033[22;33m";
+	const char *yellow = "\033[01;33m";
+	const char *blue = "\033[22;34m";
+	const char *l_blue = "\033[01;34m";
+	const char *magenta = "\033[22;35m";
+	const char *l_magenta = "\033[01;35m";
+	const char *cyan = "\033[22;36m";
+	const char *l_cyan = "\033[01;36m";
+	const char *gray = "\033[22;37m";
+	const char *white = "\033[01;37m";
+	// const char *char_ = "0";
+
+	// const char *black = "A";
+	// const char *red = "B";
+	// const char *l_red = "C";
+	// const char *green = "D";
+	// const char *l_green = "E";
+	// const char *orange = "F";
+	// const char *yellow = "G";
+	// const char *blue = "H";
+	// const char *l_blue = "I";
+	// const char *magenta = "J";
+	// const char *l_magenta = "K";
+	// const char *cyan = "L";
+	// const char *l_cyan = "M";
 	// const char *gray = "\033[22;37m";
 	// const char *white = "\033[01;37m";
-	const char *char_ = "";
 
-	const char *black = "A";
-	const char *red = "B";
-	const char *l_red = "C";
-	const char *green = "D";
-	const char *l_green = "E";
-	const char *orange = "F";
-	const char *yellow = "G";
-	const char *blue = "H";
-	const char *l_blue = "I";
-	const char *magenta = "J";
-	const char *l_magenta = "K";
-	const char *cyan = "L";
-	const char *l_cyan = "M";
-	// const char *gray = "\033[22;37m";
-	// const char *white = "\033[01;37m";
-
-	// const char *foo = "foo";
-	// const char *bar = "bar";
+	const char *foo = "foo";
+	const char *bar = "bar";
 
 	for (int i = 0; i < height; i++) {
 		for (int j = 0; j < width; j++) {
 			double x = x_start + j*dx; // current real value
 			double y = y_fin - i*dy; // current imaginary value
 
-			printf("%f, %f\n", x, y);
 			int value = mandelbrot(x,y);
-			printf("%d\n", value);
-			continue;
+			// printf("%d\n", value);
+			// continue;
 
 			// printf("%s%s", foo, bar);
 			// printf("%s", foo);
@@ -117,21 +115,21 @@ int main() {
 			// else if (value > 2) {printf("K");}
 			// else if (value > 1) {printf("L");}
 			// else {printf("M");}
-			// if (value == 100) {printf("%s", " ");}
-			// else if (value > 90) {printf("%s%s", red, char_);}
-			// else if (value > 70) {printf("%s%s", l_red, char_);}
-			// else if (value > 50) {printf("%s%s", orange, char_);}
-			// else if (value > 30) {printf("%s%s", yellow, char_);}
-			// else if (value > 20) {printf("%s%s", l_green, char_);}
-			// else if (value > 10) {printf("%s%s", green, char_);}
-			// else if (value > 5) {printf("%s%s", l_cyan, char_);}
-			// else if (value > 4) {printf("%s%s", cyan, char_);}
-			// else if (value > 3) {printf("%s%s", l_blue, char_);}
-			// else if (value > 2) {printf("%s%s", blue, char_);}
-			// else if (value > 1) {printf("%s%s", magenta, char_);}
-			// else {printf("%s%s", l_magenta, char_);}
+			if (value == 100) {printf("%s", " ");}
+			else if (value > 90) {printf("%s%s", red, char_);}
+			else if (value > 70) {printf("%s%s", l_red, char_);}
+			else if (value > 50) {printf("%s%s", orange, char_);}
+			else if (value > 30) {printf("%s%s", yellow, char_);}
+			else if (value > 20) {printf("%s%s", l_green, char_);}
+			else if (value > 10) {printf("%s%s", green, char_);}
+			else if (value > 5) {printf("%s%s", l_cyan, char_);}
+			else if (value > 4) {printf("%s%s", cyan, char_);}
+			else if (value > 3) {printf("%s%s", l_blue, char_);}
+			else if (value > 2) {printf("%s%s", blue, char_);}
+			else if (value > 1) {printf("%s%s", magenta, char_);}
+			else {printf("%s%s", l_magenta, char_);}
 			
-			// printf("\033[0m");
+			printf("\033[0m");
 		}
 
 		printf("\n");
