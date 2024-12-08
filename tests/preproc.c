34094260cf65c66566e9c9158c4627624363ec20
John Kelly <johnharrykelly@gmail.com>
2024-12-08 19:09:26 +0000

    New tests, `no-compile` mode for tests

--- tests/preproc.c
+++ tests/preproc.c
@@ -1,11 +1,17 @@
+// expected value: 8
 
-#define FOO
+#define FOO \
+\
+\
+(8)
 
 int main() {
-  int a = 8;
+  int a = FOO;
 
   /* logic here is something */
 
   float f = 8.3E9;
   int k = 8777; // hello
+
+  return a;
 }
