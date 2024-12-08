7b9e0299c1cd3db419674092c11225593d3994ec
John Kelly <johnharrykelly@gmail.com>
2024-12-08 19:12:53 +0000

    All tests pass

--- tests/run.sh
+++ tests/run.sh
@@ -22,34 +22,34 @@ for file in $(find $(dirname $0) -name '*.c' -print | sort); do
     else
       echo "TEST PASSED: compilation failed as expected"
     fi
-    continue
-  fi
-
-  expected=$(grep -i "Expected value:" $file | head -1 | grep -Eo '[0-9]+')
-  stdin=$(grep -i "stdin" $file | head -1 | sed -n 's/^\/\/ stdin: //p')
-  stdout=$(grep -i "stdout" $file | head -1 | sed -n 's/^\/\/ stdout: //p')
-
-  if [ -z "$expected" ]; then
-    expected="0"
-  fi
-
-  if ! ./build/jcc $file >/dev/null 2>&1; then
-    echo "compilation failed!"
-    exit -1
-  fi
-
-  output=$(echo "$stdin" | ./a.out)
-
-  result=$?
-  if [ "$result" != "$expected" ]; then
-    echo "TEST FAILED: expected return code $expected, got $result"
-    break
-  elif [ "$output" != "$stdout" ]; then
-    echo "TEST FAILED: expected stdout '$stdout', got '$output'"
-    break
   else
-    echo "TEST PASSED"
+    expected=$(grep -i "Expected value:" $file | head -1 | grep -Eo '[0-9]+')
+    stdin=$(grep -i "stdin" $file | head -1 | sed -n 's/^\/\/ stdin: //p')
+    stdout=$(grep -i "stdout" $file | head -1 | sed -n 's/^\/\/ stdout: //p')
+
+    if [ -z "$expected" ]; then
+      expected="0"
+    fi
+
+    if ! ./build/jcc $file >/dev/null 2>&1; then
+      echo "compilation failed!"
+      exit -1
+    fi
+
+    output=$(echo "$stdin" | ./a.out)
+
+    result=$?
+    if [ "$result" != "$expected" ]; then
+      echo "TEST FAILED: expected return code $expected, got $result"
+      break
+    elif [ "$output" != "$stdout" ]; then
+      echo "TEST FAILED: expected stdout '$stdout', got '$output'"
+      break
+    else
+      echo "TEST PASSED"
+    fi
   fi
+
 
   echo -e "\n"
 done
