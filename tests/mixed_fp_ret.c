b830953c91a8573bbc98ae7b200432426e49f01e
John Kelly <johnharrykelly@gmail.com>
2025-02-21 05:55:17 +0000

    Create mixed_fp_ret.c

---
+++ tests/mixed_fp_ret.c
@@ -0,0 +1,16 @@
+// expected value: 3
+
+struct S { float x; double y; };
+
+struct S f(double d)
+{
+    struct S s;
+    s.x = d;
+    s.y = d;
+    return s;
+}
+
+int main() {
+  struct S s = f(1.5);
+  return s.x + s.y;
+}
