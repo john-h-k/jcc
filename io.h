d7d572f8a5a8a6001fdd38db6913b61301ce65bf
John Kelly <johnharrykelly@gmail.com>
2025-02-24 16:32:20 +0000

    Remove allocs from `io`

--- io.h
+++ io.h
@@ -1,14 +1,16 @@
 #ifndef IO_H
 #define IO_H
 
+#include "alloc.h"
+
 struct path_components {
   const char *dir, *file;
 };
 
-struct path_components path_components(const char *path);
-char *path_combine(const char *l, const char *r);
-char *path_replace_ext(const char *path, const char *ext);
-char *path_add_ext(const char *path, const char *ext);
-char *read_file(const char *path);
+struct path_components path_components(struct arena_allocator *arena, const char *path);
+char *path_combine(struct arena_allocator *arena, const char *l, const char *r);
+char *path_replace_ext(struct arena_allocator *arena, const char *path, const char *ext);
+char *path_add_ext(struct arena_allocator *arena, const char *path, const char *ext);
+char *read_file(struct arena_allocator *arena, const char *path);
 
 #endif
