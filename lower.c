a9d6f697889e071c1b4119597ae0853672c6141c
John Kelly <johnharrykelly@gmail.com>
2025-02-24 14:10:19 +0000

    Fixes

--- lower.c
+++ lower.c
@@ -1184,6 +1184,7 @@ void lower(struct ir_unit *unit, const struct target *target) {
       prune_basicblocks(func);
 
       remove_critical_edges(func);
+      eliminate_redundant_ops(func);
     }
     }
     glb = glb->succ;
