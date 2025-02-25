635802f0e6b5c80353eea37835cfab87f9455666
John Kelly <johnharrykelly@gmail.com>
2025-02-25 17:24:51 +0000

    Experiment: new test runner

--- tests/ci-run.sh
+++ tests/ci-run.sh
@@ -19,28 +19,24 @@ arch=${arch/arm64/aarch64}
 
 tm="Tue Dec 10 10:04:33 2024"
 
-for file in $(find $(dirname $0) -name '*.c' -print | sort); do
+for file in $(find $(dirname $0)/tests -name '*.c' -print | sort); do
   if [[ $file == *"/programs/"* ]]; then
     continue
   fi
 
-  echo "Testing $file..."
-
   first_line=$(head -n 1 "$file")
   if [[ "$first_line" == "// no-compile" ]]; then
     if ./build/jcc "$@" -std=c23 -tm "$tm" "$file" >/dev/null 2>&1; then
-      echo "TEST FAILED: expected compilation to fail, but it succeeded"
+      echo "TEST FAILED: '$file' expected compilation to fail, but it succeeded"
       exit -1
-    else
-      echo "TEST PASSED: compilation failed as expected"
     fi
   else
     target_arch=$(grep -i "arch" $file | head -1 | sed -n 's/^\/\/ arch: //p')
-    arch=rv32i
     if [[ -n $target_arch && $target_arch != $arch ]]; then
       echo "Skipping test (arch=$arch, test_arch=$target_arch)"
       continue
     fi
+
 
     expected=$(grep -i "Expected value:" $file | head -1 | grep -Eo '[0-9]+')
     stdin=$(grep -i "stdin" $file | head -1 | sed -n 's/^\/\/ stdin: //p')
@@ -50,33 +46,37 @@ for file in $(find $(dirname $0) -name '*.c' -print | sort); do
       expected="0"
     fi
 
-    if ! ./build/jcc "$@" -std=c23 -tm "$tm" $file >/dev/null 2>&1; then
-      echo "compilation failed!"
-
-      ./build/jcc "$@" -std=c23 -tm "$tm" $file
+    if ! ./build/jcc "$@" -std=c23 -tm "$tm" $file -Lasm >build.txt 2>&1; then
+      echo "TEST FAILED: '$file' compilation failed!"
+      echo "BUILD:"
+      cat build.txt
+      echo ""
       exit -1
     fi
 
-    # output=$(echo "$stdin" | spike pk a.out)
-    # result=$?
-    # output=$(echo "$output" | tail -n +2)
-    output=$(echo "$stdin" | ./a.out)
-    result=$?
-    # output=$(echo "$stdin" | riscy a.out)
-    # result=$?
- 
+    # FIXME: this is hacky
+    if [ -z "$RUNNER" ]; then
+      output=$(echo "$stdin" | ./a.out)
+      result=$?
+    else
+      output=$(echo "$stdin" | $RUNNER a.out)
+      result=$?
+    fi
+
     if [ "$result" != "$expected" ]; then
-      echo "TEST FAILED: expected return code $expected, got $result"
+      echo "TEST FAILED: '$file' expected return code $expected, got $result"
+      echo "BUILD:"
+      cat build.txt
+      echo ""
+      echo "OUTPUT:"
+      echo "$output"
+      echo ""
       exit -1
     elif [ "$output" != "$stdout" ]; then
-      echo "TEST FAILED: expected stdout '$stdout', got '$output'"
+      echo "TEST FAILED: '$file' expected stdout '$stdout', got '$output'"
       exit -1
-    else
-      echo "TEST PASSED"
     fi
   fi
-
-
-  echo -e "\n"
 done
+
 
--- tests/tests/bitfield.c
+++ tests/tests/bitfield.c
@@ -15,11 +15,6 @@ int main() {
     .c = 0
   };
 
-  unsigned *c = &f;
-  c[0] |= 0xFFFFFFFE;
-  c[1] |= 0x7FFFFFE0;
-  c[2] |= 0x7FFFFFFE;
-
   if (f.a != 1) {
     return 1;
   }
