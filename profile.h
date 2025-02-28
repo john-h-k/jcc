12d6d5df7f1cbe9bb55f069c98d01a708f4335fe
John Kelly <johnharrykelly@gmail.com>
2025-02-28 17:08:45 +0000

    Use `mach_absolute_time` where possible

--- profile.h
+++ profile.h
@@ -1,23 +1,29 @@
 #ifndef PROFILE_H
 #define PROFILE_H
 
+#include "util.h"
 #include <stdio.h>
 
 // this is all done by globals so we don't need to pass around stuff
 // (A) watch out for thread safety
 // (B) is this bad?
 
-#define PROFILE_BEGIN(name) \
-    struct profiler_region profiler_ ## name ## _region = profiler_begin_region(# name);
-#define PROFILE_END(name) \
-    profiler_end_region(profiler_ ## name ## _region);
+#define PROFILE_BEGIN(name)                                                    \
+  struct profiler_region profiler_##name##_region =                            \
+      profiler_begin_region(#name);
+#define PROFILE_END(name) profiler_end_region(profiler_##name##_region);
+
+#define PROFILE(name, code)                                                    \
+    PROFILE_BEGIN(name);                                                       \
+      code;                                                                    \
+    PROFILE_END(name);                                                         \
 
 struct profiler_region {
   size_t idx;
 };
 
-struct profiler_region profiler_begin_region(const char *name);
-void profiler_end_region(struct profiler_region region);
+NOINLINE struct profiler_region profiler_begin_region(const char *name);
+NOINLINE void profiler_end_region(struct profiler_region region);
 
 void profiler_print(FILE *file);
 
