a65102957a4648f554cf0460c674a0d593096270
John Kelly <johnharrykelly@gmail.com>
2025-03-02 00:23:05 +0000

    Update many_args.c

--- tests/many_args.c
+++ tests/many_args.c
@@ -1,4 +1,4 @@
-// expected value: 0
+// expected value: 66
 
 int foo(int a, int b, int c, int d, int e, int f, int g, int h, int i, int j, int k) {
   return a + b + c + d + e + f + g + h + i + j + k;
@@ -7,5 +7,5 @@ int foo(int a, int b, int c, int d, int e, int f, int g, int h, int i, int j, in
 int main() {
   int res = foo(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11);
 
-  return res != 66;
+  return res;
 }
