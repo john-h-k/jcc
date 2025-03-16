7ede9630fcca1c291e8ccb66c8c4511b4d412320
John Kelly <johnharrykelly@gmail.com>
2025-03-16 05:56:04 +0000

    Update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -63,7 +63,7 @@ if has_tool bash && has_tool cmake && [ -z "JCC_FORCE_SIMPLE_BUILD" ]; then
     output="build/jcc"
     ./jcc.sh build
 else
-    printf "%b\n" "${BOLD} Bash is not installed; reverting to a simple build...$RESET"
+    printf "%b\n" "${BOLD} Bash/CMake is not installed; reverting to a simple build...$RESET"
     # if bash isn't installed, manually do a simple build
 
     mkdir -p build
