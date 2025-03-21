2ce12cac9ce5445f337c6b53a43e8b8e4c46c17a
John Kelly <johnharrykelly@gmail.com>
2025-03-21 14:18:40 +0000

    Respect 1 fp/1 int req for rv32imd

---
+++ tests/vec2i_add.c
@@ -0,0 +1,46 @@
+// expected value: 0
+
+struct vec2 {
+  float a;
+  int b;
+};
+
+struct vec2 add(struct vec2 a, struct vec2 b) {
+  a.a += b.a;
+  a.b += b.b;
+  return a;
+}
+
+struct vec2 add_tmp(struct vec2 a, struct vec2 b) {
+  struct vec2 tmp;
+  tmp.a = a.a + b.a;
+  tmp.b = a.b + b.b;
+  return tmp;
+}
+
+int main() {
+  struct vec2 l = { 1, 2, };
+  struct vec2 r = { 0, 2, };
+
+  struct vec2 sum = add(l, r);
+
+  if (sum.a != 1) {
+    return 1;
+  }
+
+  if (sum.b != 4) {
+    return 2;
+  }
+
+  sum = add_tmp(sum, l);
+
+  if (sum.a != 2) {
+    return 2;
+  }
+
+  if (sum.b != 6) {
+    return 3;
+  }
+}
+
+
