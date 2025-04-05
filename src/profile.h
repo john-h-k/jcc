82b5e03a4aed08215fdc7ca071ee459f931c5126
John Kelly <johnharrykelly@gmail.com>
2025-04-05 21:46:57 +0100

    Profiler multi-regions

--- src/profile.h
+++ src/profile.h
@@ -14,14 +14,23 @@
       profiler_begin_region(#name);
 #define PROFILE_END(name) profiler_end_region(profiler_##name##_region);
 
+#define PROFILE_BEGIN_MULTI(name)                                                    \
+  struct profiler_region profiler_##name##_region =                            \
+      profiler_begin_multi_region(#name);
+#define PROFILE_END_MULTI(name) profiler_end_multi_region(profiler_##name##_region);
+
 #define PROFILE(name, code)                                                    \
   PROFILE_BEGIN(name);                                                         \
   code;                                                                        \
   PROFILE_END(name);
 
 struct profiler_region {
+  const char *name;
   size_t idx;
 };
+
+NOINLINE struct profiler_region profiler_begin_multi_region(const char *name);
+NOINLINE void profiler_end_multi_region(struct profiler_region region);
 
 NOINLINE struct profiler_region profiler_begin_region(const char *name);
 NOINLINE void profiler_end_region(struct profiler_region region);
