4c29b979a5e5628ff18b9917a0362f4c1c295aed
John Kelly <johnharrykelly@gmail.com>
2025-04-17 14:27:04 +0100

    Update jcc.sh

--- jcc.sh
+++ jcc.sh
@@ -481,11 +481,11 @@ test-all() {
 ci-test() {
     # timeout -k 30m 30m
     # opts disabled for now
-    JCC_BUILD_FLAGS="address" _test --quiet "$@"
+    JCC_BUILD_FLAGS="--san address" _test --quiet "$@"
 
     if [[ "$(uname)" != "darwin" ]]; then
-        JCC_BUILD_FLAGS="memory" _test --quiet "$@"
-        JCC_BUILD_FLAGS="thread" _test --quiet "$@"
+        JCC_BUILD_FLAGS="--san memory" _test --quiet "$@"
+        JCC_BUILD_FLAGS="--san thread" _test --quiet "$@"
     fi
     # _test --quiet --arg-group -O0 --arg-group -O1 --arg-group -O2 --arg-group -O3 "$@"
 }
