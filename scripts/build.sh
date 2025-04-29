90fe84f65b7240245acda5489ef1d736909d19b5
John Kelly <johnharrykelly@gmail.com>
2025-04-29 15:21:25 +0100

    Use clang for memory sanitizer

--- scripts/build.sh
+++ scripts/build.sh
@@ -182,7 +182,7 @@ configure() {
 
     if [ -z "$cc" ]; then
         # if ASan on try and use clang
-        if [[ "$mode" == "Debug" ]] && [[ "$san" != "address" ]] && command -v clang &>/dev/null; then
+        if [[ "$mode" == "Debug" ]] && [[ "$san" == "address" ]] && command -v clang &>/dev/null; then
            { cc --version 2>/dev/null | grep clang; } && cc="cc" || cc="clang"
         else
             cc="cc"
